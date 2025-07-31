e6a4640a14bb55efbfe85e275e487d5c9b148fe0
Loard <zaidangaming47@gmail.com>
2025-07-31 21:16:03 +0700

    Create dor.js

---
+++ dor.js
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>HACKED BY MΞТΛS_PŁФłÐ</title>
+  <style>
+    body {
+      background-color: black;
+      color: white;
+      font-family: monospace;
+      text-align: center;
+      padding-top: 50px;
+    }
+
+    h1 {
+      color: red;
+      font-size: 28px;
+      margin-bottom: 25px;
+      text-shadow: 0 0 10px red;
+    }
+
+    img {
+      width: 220px;
+      height: 220px;
+      border-radius: 50%;
+      object-fit: cover;
+      margin-bottom: 30px;
+    }
+
+    .russian-text {
+      color: #ccc;
+      font-size: 16px;
+      margin: 0 20px 10px;
+      text-shadow: 0 0 5px white;
+    }
+
+    .hashtag {
+      color: red;
+      font-weight: bold;
+      font-size: 18px;
+      margin-top: 10px;
+      text-shadow: 0 0 5px red;
+    }
+
+    .audio-container {
+      margin-top: 40px;
+    }
+
+    audio {
+      width: 300px;
+      height: 40px;
+      border-radius: 25px;
+      background: white;
+      box-shadow: 0 0 20px red;
+    }
+
+  </style>
+</head>
+<body>
+
+  <h1>HACKED BY<br>MΞТΛS_PŁФłÐ</h1>
+  <img src="https://files.catbox.moe/p6er02.png" alt="Foto METAS_PLOID">
+
+  <p class="russian-text">
+    каждый чувствует ложные обещания, которые всегда называются верными словами, но уязвимость становится свидетелем
+  </p>
+  <div class="hashtag">#lolsystem</div>
+
+  <div class="audio-container">
+    <audio controls loop>
+      <source src="https://files.catbox.moe/4i75zy.mp3" type="audio/mpeg">
+    </audio>
+  </div>
+
+</body>
+</html>
